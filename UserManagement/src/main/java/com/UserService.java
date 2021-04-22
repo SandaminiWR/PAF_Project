1ba6332cd0a78c2e1ade1980437b650c18606fee
SandaminiWR <it19192710@my.sliit.lk>
2021-04-22 09:33:40 +0530

    Add insertUser method

--- UserManagement/src/main/java/com/UserService.java
+++ UserManagement/src/main/java/com/UserService.java
@@ -24,4 +24,22 @@ public class UserService {
 		 { 
 		 return usObj.readUser(); 
 		 }
+	
+		@POST
+		@Path("/") 
+		@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+		@Produces(MediaType.TEXT_PLAIN)
+
+	public String insertUSer
+				(@FormParam("name") String name, 
+				 @FormParam("phone") String phone, 
+				 @FormParam("address") String address, 
+				 @FormParam("password") String password,
+	 			 @FormParam("confirmpassword") String confirmpassword)
+				{
+			String output = usObj.insertUser(name, phone, address, address, password, confirmpassword);
+				return output;
+				}
 }
+
+
